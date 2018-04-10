220b4bd761e986b9b088b3622627a2f679476483
Patrick Gourdet <patrickgourdet79@gmail.com>
2018-04-09 23:21:41 -0400

    Routes work on edit user

--- client/api/controllers/users.controller.js
+++ client/api/controllers/users.controller.js
@@ -48,7 +48,7 @@ oracledb.getConnection(connectionInfo,
         if(err){
           console.log(err.message);
         }else {
-          console.log("GET /profiels : Connection Released");
+          console.log("GET/profiels : Connection Released");
         }
       })
 
@@ -60,12 +60,43 @@ oracledb.getConnection(connectionInfo,
 
 module.exports.usersGetOne =function(req,res){
 "use strict"
+
 oracledb.getConnection(connectionInfo,
   function(err, connection)
   {
-    if(err){
-
+    if (err) {
+      res.set('Content-Type','application/json');
+      res.status(500).send(JSON.stringify({
+        status: 500,
+        message: "Error connection to DB",
+        details: err.message
+      }));
+      return;
     }
+    connection.execute("SELECT * FROM USERS WHERE USER_NAME = :USER_NAME",[req.params.USER_NAME],{
+      outFormat: oracledb.ARRAY
+      }, function (err, result){
+        if(err || result.rows.lenth < 1){
+          res.set('Content-Type', 'application/jason');
+          let status = err ? 500 : 404;
+          res.status(status).send(JSON.stringify({
+            status: status,
+            message: err ? "ERROR GETING THE USER PROFILE": "USER DOESNT EXIST",
+            detailed_message: err ? err.message : ""
+          }));
+        }else {
+          res.contentType('application/json').status(200).send(JSON.stringify(result.row));
+        }
+      connection.release((err)=>{
+        if(err){
+          console.log(err.message);
+        }else {
+          console.log("GET ONE USER/profiels : Connection Released");
+        }
+      })
+      }
+
+    )
 
     })
 };
@@ -82,6 +113,14 @@ oracledb.getConnection(connectionInfo,
 
 module.exports.userEdit =function(req,res){
 "use strict"
+  if("application/json" !== req.get('Content-Type')){
+  res.set('Content-Type','application/json').status(415).send(JSON.stringify({
+    status: 415,
+    message: "Wong content-type . ONly applicaton/json is supported",
+    detailed_message: null,
+  }));
+  return;
+  }
 oracledb.getConnection(connectionInfo,
   function(err, connection)
   {
